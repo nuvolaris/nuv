5e984323bafff188123b0dc7b7c94f4aa72b9b5f
Michele Sciabarra <michele@nuvolaris.io>
2023-03-18 13:34:02 +0000

    eksctl and archive support

--- bin/windows/setup.cmd
+++ bin/windows/setup.cmd
@@ -1,2 +1,2 @@
 set PATH=%CD%\wix\tools;%PATH%
-go-msi make -p wix.json -a amd64 -m nuv.msi -l ../../LICENSE --version 0.3.0 --src templates
+go-msi make -p wix.json -a amd64 -m nuv.msi -l LICENSE --version 0.3.0 --src templates
