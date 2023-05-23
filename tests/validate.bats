d2cb23653ae862a3dde60ca11256aa8c6af2100c
Giuseppe De Palma <dpl.gsp@tutanota.com>
2023-05-23 12:45:38 +0200

    feat: implement validate tool

--- tests/validate.bats
+++ tests/validate.bats
@@ -21,3 +21,61 @@ setup() {
     export NO_COLOR=1
 }
 
+@test "nuv validate help" {
+    run nuv -validate
+    assert_line "Usage:"
+
+    run nuv -validate -h
+    assert_line "Usage:"
+}
+
+@test "nuv validate email" {
+    run nuv -validate -m example@email.com
+    assert_line "'example@email.com' is a valid email address."
+
+    run nuv -validate -m example
+    assert_line "'example' is NOT a valid email address."
+}
+
+@test "nuv validate number" {
+    run nuv -validate -n 123
+    assert_line "'123' is a valid number."
+
+    run nuv -validate -n 123.456
+    assert_line "'123.456' is a valid number."
+
+    run nuv -validate -n abc
+    assert_line "'abc' is NOT a valid number."
+}
+
+@test "nuv validate with custom regex" {
+    run nuv -validate -r '^[a-z]+$' abc
+    assert_line "'abc' matches the regex."
+
+    run nuv -validate -r '^[a-z]+$' 123
+    assert_line "'123' does NOT match the regex."
+}
+
+@test "nuv validate on env vars" {
+    run nuv -validate -e -n TEST_ENV_VAR
+    assert_line "The variable 'TEST_ENV_VAR' is not set."
+
+    export TEST_ENV_VAR=123
+    run nuv -validate -e -n TEST_ENV_VAR
+    assert_line "'123' from the variable 'TEST_ENV_VAR' is a valid number."
+
+    run nuv -validate -e -m TEST_ENV_VAR
+    assert_line "'123' from the variable 'TEST_ENV_VAR' is NOT a valid email address."
+
+    export TEST_ENV_VAR=example@gmail.com
+    run nuv -validate -e -m TEST_ENV_VAR
+    assert_line "'example@gmail.com' from the variable 'TEST_ENV_VAR' is a valid email address."
+
+    export TEST_ENV_VAR=abc
+    run nuv -validate -e -r '^[a-z]+$' TEST_ENV_VAR
+    assert_line "'abc' from the variable 'TEST_ENV_VAR' matches the regex."
+
+    export TEST_ENV_VAR=123
+    run nuv -validate -e -r '^[a-z]+$' TEST_ENV_VAR
+    assert_line "'123' from the variable 'TEST_ENV_VAR' does NOT match the regex."
+}
--- tools/validate.go
+++ tools/validate.go
@@ -19,17 +19,129 @@ package tools
 
 import (
 	"flag"
+	"fmt"
 	"os"
+	"regexp"
+	"strconv"
 )
 
 func validateTool() error {
 	flag := flag.NewFlagSet("validate", flag.ContinueOnError)
+
+	flag.Usage = printValidateUsage
+
+	helpHalf := flag.Bool("h", false, "")
+	envFlag := flag.Bool("e", false, "")
+	mailFlag := flag.Bool("m", false, "")
+	numberFlag := flag.Bool("n", false, "")
+	regexFlag := flag.String("r", "", "")
 
 	err := flag.Parse(os.Args[1:])
 	if err != nil {
 		return err
 	}
 
-	return nil
+	if *helpHalf {
+		flag.Usage()
+		return nil
+	}
 
+	if flag.NArg() != 1 {
+		flag.Usage()
+		return nil
+	}
+
+	arg := flag.Arg(0)
+	value := arg
+
+	if *envFlag {
+		value = os.Getenv(arg)
+		if value == "" {
+			fmt.Printf("The variable '%s' is not set.\n", arg)
+			return nil
+		}
+	}
+
+	if *mailFlag {
+		if isValidEmail(value) {
+			fmt.Printf("'%s' %sis a valid email address.\n", value, envVarMsg(*envFlag, arg))
+		} else {
+			fmt.Printf("'%s' %sis NOT a valid email address.\n", value, envVarMsg(*envFlag, arg))
+		}
+		return nil
+	}
+
+	if *numberFlag {
+		if isValidNumber(value) {
+			fmt.Printf("'%s' %sis a valid number.\n", value, envVarMsg(*envFlag, arg))
+		} else {
+			fmt.Printf("'%s' %sis NOT a valid number.\n", value, envVarMsg(*envFlag, arg))
+		}
+		return nil
+	}
+
+	if *regexFlag != "" {
+		valid, err := isValidByRegex(value, *regexFlag)
+		if err != nil {
+			return err
+		}
+
+		if valid {
+			fmt.Printf("'%s' %smatches the regex.\n", value, envVarMsg(*envFlag, arg))
+		} else {
+			fmt.Printf("'%s' %sdoes NOT match the regex.\n", value, envVarMsg(*envFlag, arg))
+		}
+		return nil
+	}
+
+	return nil
+}
+
+func envVarMsg(envFlag bool, name string) string {
+	if envFlag {
+		return "from the variable '" + name + "' "
+	}
+	return ""
+}
+
+func isValidNumber(number string) bool {
+	_, err := strconv.ParseFloat(number, 64)
+	return err == nil
+}
+
+func isValidEmail(email string) bool {
+	// Regular expression pattern for email validation
+	pattern := `^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`
+
+	// Create a regular expression object
+	regExp := regexp.MustCompile(pattern)
+
+	// Use the regular expression to match the email string
+	return regExp.MatchString(email)
+}
+
+func isValidByRegex(value string, regex string) (bool, error) {
+	// Create a regular expression object
+	regExp, err := regexp.Compile(regex)
+	if err != nil {
+		return false, err
+	}
+
+	// Use the regular expression to match the email string
+	return regExp.MatchString(value), nil
+}
+
+func printValidateUsage() {
+	fmt.Println(`Usage:
+nuv -validate [-e] [-m | -n | -r <regex>] <value>
+
+Check if a value is valid according to the given constraints.
+If -e is specified, the value is retrieved from the environment variable with the given name.
+
+Options:
+	-h		Print this help message.
+	-e		The value is retrieved from the environment variable with the given name.
+	-m		Check if the value is a valid email address.
+	-n		Check if the value is a number.
+	-r		Check if the value matches the given regular expression.`)
 }
